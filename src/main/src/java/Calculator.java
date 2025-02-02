b526c0178fd130509e16cf26c484a4e14c470073
AlexandrLikhanov <u2t1frr@gmail.com>
2025-02-02 11:57:28 +0900

    refactor of the approach to operations (version-1.1)

--- src/main/src/java/Calculator.java
+++ src/main/src/java/Calculator.java
@@ -5,54 +5,68 @@ public class Calculator {
         System.out.println("Приложение Calculate");
         System.out.println("version - 1.0");
 
-        boolean nextOperation = true;
+        boolean isRunning = true;
         Scanner scanner = new Scanner(System.in);
 
-        while (nextOperation) {
-            System.out.println("Введите первое число: ");
+        while (isRunning) {
+            System.out.println("\nВыберите операцию:");
+            System.out.println("1. Сложение (+)");
+            System.out.println("2. Вычитание (-)");
+            System.out.println("3. Умножение (*)");
+            System.out.println("4. Деление (/)");
+            System.out.println("5. Выйти");
+            System.out.print("Ваш выбор: ");
+
+            int choice = scanner.nextInt();
+
+            if (choice == 5) {
+                isRunning = false;
+                System.out.println("Выход из калькулятора. До свидания!");
+                continue;
+            }
+
+            if (choice < 1 || choice > 4) {
+                System.out.println("Ошибка: неверный выбор операции. Попробуйте снова.");
+                continue;
+            }
+
+            // Запрашиваем числа
+            System.out.print("Введите первое число: ");
             double num1 = scanner.nextDouble();
 
-            System.out.printf("Введите операцию (+, -, *, /): ");
-            char operation = scanner.next().charAt(0);
-
-            System.out.println("Введите второе число: ");
+            System.out.print("Введите второе число: ");
             double num2 = scanner.nextDouble();
 
             double result = 0;
+            String operation = "";
 
-            switch (operation) {
-                case '+':
+            switch (choice) {
+                case 1:
                     result = num1 + num2;
+                    operation = "+";
                     break;
-                case '-':
+                case 2:
                     result = num1 - num2;
+                    operation = "-";
                     break;
-                case '*':
+                case 3:
                     result = num1 * num2;
+                    operation = "*";
                     break;
-                case '/':
+                case 4:
                     if (num2 != 0) {
                         result = num1 / num2;
+                        operation = "/";
                     } else {
                         System.out.println("Ошибка: деление на ноль!");
-                        return;
+                        continue;
                     }
                     break;
-                default:
-                    System.out.println("Ошибка: неизвестная операция!");
-                    return;
-            }
-            System.out.println("Результат " + result);
-
-            System.out.println("Хотите продолжить? yes / no");
-            String choice = scanner.next();
-            if (choice.equalsIgnoreCase("no")) {
-                nextOperation = false;
-            } else if (!choice.equalsIgnoreCase("yes")) {
-                System.out.println("Неверный ввод. Программа завершена.");
-                nextOperation = false;
             }
 
+            System.out.println("\nРезультат: " + num1 + " " + operation + " " + num2 + " = " + result);
         }
+
+        scanner.close();
     }
 }
